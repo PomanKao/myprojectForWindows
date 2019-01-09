526277e858547a2eb2f12d08463f990b0db50151
Poman <ayujolin520@gmail.com>
2019-01-09 12:30:46 +0800

    init

---
+++ src/main/java/com/pomankao/Hello.java
@@ -0,0 +1,9 @@
+package com.pomankao;
+
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.println("Hello world");
+        Person person = new Person();
+        person.hello();
+    }
+}
---
+++ src/main/kotlin/com/pomankao/Hello.kt
@@ -0,0 +1,14 @@
+package com.pomankao
+
+fun main(args: Array<String>) {
+//    println("Hello Kotlin")
+    val human = Human()
+    human.hello()
+}
+
+
+class Human {
+    fun hello() {
+        println("Hello kotlin")
+    }
+}
